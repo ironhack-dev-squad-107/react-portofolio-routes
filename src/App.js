87613297e20b350ac49503ece99a10178659b882
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2019-02-21 09:46:37 +0100

    Remove default React content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>Portfolio Site of Nizar</h1>
+
+          <nav>Under construction...</nav>
         </header>
+        Content that changes between pages...
+        <footer>
+          <p>Made with 🤖 at Ironhack</p>
+        </footer>
       </div>
     );
   }
